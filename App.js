812513ab1ae216d9a62073d192f96532b68bd272
Eren Nasıroğlu <erenasiroglu1@gmail.com>
2024-11-10 17:12:14 +0300

    feat: create the project

---
+++ App.js
@@ -0,0 +1,10 @@
+import {Text, SafeAreaView} from 'react-native';
+import React from 'react';
+
+export default function App() {
+  return (
+    <SafeAreaView className="bg-red-200">
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+}
