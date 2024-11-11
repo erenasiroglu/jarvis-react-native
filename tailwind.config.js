09508b2aeb320453a84e5c30cad520a2ff4f34d8
Eren Nasıroğlu <erenasiroglu1@gmail.com>
2024-11-11 09:29:51 +0300

    feat: create the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [
-    './App.{js,jsx,ts,tsx}',
-    './src/screens/**/*.{js,jsx,ts,tsx}',
-    './src/components/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
